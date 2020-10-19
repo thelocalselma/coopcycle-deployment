ec4b126b688a9685926357ba7bdd7bba5ce1b375
Jason Prado <jason.prado@gmail.com>
2020-10-19 21:14:23 +0000

    Convert pods to deployments
    * Convert OSRM/Tile38 pods to deployments
    * Add MJML deployment to get email working
    * Fix nginx config to support websockets

--- scripts/build-docker-images.sh
+++ scripts/build-docker-images.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 help()
 {
   echo "Performs a full production build of docker images for:"
@@ -14,28 +16,45 @@ help()
   echo "  -h  show this help text"
   echo "  -p [prefix]  Tag the built containers with the specified prefix,"
   echo "               e.g. -p coopcycle => coopcycle/coopcycle-web"
+  echo "  -P  push the built images to the container registry"
 }
 
 cd $(dirname "$0")
 SCRIPT_DIR=$(pwd)
 APP_DIR="$SCRIPT_DIR"/../..
+cd -
 
 CONTAINER_TAG_PREFIX=jasonprado
+UPLOAD=false
+COOPCYCLE_WEB_PATH="$SCRIPT_DIR"/../../coopcycle-web
 
-while getopts "hp:" arg; do
+while getopts "hc:Pp:" arg; do
   case $arg in
     h)
       help
       exit
       ;;
+    c)
+      COOPCYCLE_WEB_PATH=$OPTARG
+      ;;
     p)
       CONTAINER_TAG_PREFIX=$OPTARG
+      ;;
+    P)
+      UPLOAD=true
       ;;
   esac
 done
 
 set -x
 
-cd "$APP_DIR"
-docker build ../coopcycle-web -f ../coopcycle-web/docker/php/Dockerfile -t "$CONTAINER_TAG_PREFIX/coopcycle-web"
-docker build ../coopcycle-web -f ../coopcycle-web/docker/locationserver/Dockerfile -t "$CONTAINER_TAG_PREFIX/coopcycle-locationserver"
+cd "$COOPCYCLE_WEB_PATH"
+docker build . -f ./docker/php/Dockerfile -t "$CONTAINER_TAG_PREFIX/coopcycle-web"
+docker build . -f ./docker/locationserver/Dockerfile -t "$CONTAINER_TAG_PREFIX/coopcycle-locationserver"
+docker build . -f ./docker/php_worker/Dockerfile -t "$CONTAINER_TAG_PREFIX/coopcycle-web-worker"
+
+if [ "$UPLOAD" = true ]; then
+  docker push "$CONTAINER_TAG_PREFIX/coopcycle-web"
+  docker push "$CONTAINER_TAG_PREFIX/coopcycle-locationserver"
+  docker push "$CONTAINER_TAG_PREFIX/coopcycle-web-worker"
+fi
